ad39e86f16e865b24f9833475dfc0ac0b5451211
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-07-17 00:38:41 -0400

    feat: add `animate.py` that currently does nothing

---
+++ scripts/animate.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import argparse
+
+def main(args):
+    pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    args = parser.parse_args()
+    main(args)
