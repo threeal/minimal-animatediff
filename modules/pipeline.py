999e19b510252d5b115c22bf4f49afce8729a841
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-07-18 06:44:06 -0400

    feat: add `motion_module.load_state_dict` function

--- modules/pipeline.py
+++ modules/pipeline.py
@@ -1,12 +1,13 @@
 from diffusers import DDIMScheduler
 
 from deps.AnimateDiff.animatediff.pipelines.pipeline_animation import AnimationPipeline
+import modules.motion_module as mm
 import modules.stable_diffusion as sd
 
 
 def create_animation_pipeline():
     print('Creating animation pipeline...')
-    return AnimationPipeline(
+    pipeline = AnimationPipeline(
         text_encoder=sd.load_text_encoder(),
         tokenizer=sd.load_tokenizer(),
         vae=sd.load_vae(),
@@ -15,8 +16,17 @@ def create_animation_pipeline():
             'num_train_timesteps': 1000,
             'beta_start': 0.00085,
             'beta_end': 0.012,
-            'beta_schedule': "linear",
+            'beta_schedule': 'linear',
             'steps_offset': 1,
             'clip_sample': False
         }),
     )
+
+    pipeline.to("cuda")
+
+    print('Loading motion module to the animation pipeline...')
+    _, unexpected = pipeline.unet.load_state_dict(mm.load_state_dict(), strict=False)
+    if len(unexpected) > 0:
+        exit('Failed to load motion module to the animation pipeline!')
+
+    return pipeline
