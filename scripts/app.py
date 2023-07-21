86e3c219ffc876191b246f06d7bb9a91f31a5231
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-07-21 06:23:37 -0400

    feat(app): create animation pipeline before generating the image

--- scripts/app.py
+++ scripts/app.py
@@ -5,27 +5,25 @@ import torch
 from deps.AnimateDiff.animatediff.utils.util import save_videos_grid
 
 
-def generate(prompt):
+if __name__ == '__main__':
     pipeline = create_animation_pipeline()
 
-    torch.manual_seed(16372571278361863751)
+    def generate(prompt):
+        torch.manual_seed(16372571278361863751)
 
-    sample = pipeline(
-        prompt,
-        negative_prompt     = '',
-        num_inference_steps = 25,
-        guidance_scale      = 7.5,
-        width               = 512,
-        height              = 512,
-        video_length        = 16,
-    ).videos
+        sample = pipeline(
+            prompt,
+            negative_prompt     = '',
+            num_inference_steps = 25,
+            guidance_scale      = 7.5,
+            width               = 512,
+            height              = 512,
+            video_length        = 16,
+        ).videos
 
-    save_videos_grid(sample, 'samples/sample.gif')
+        save_videos_grid(sample, 'samples/sample.gif')
+        return 'samples/sample.gif'
 
-    return 'samples/sample.gif'
-
-
-if __name__ == '__main__':
     app = gradio.Interface(
         fn=generate,
         inputs=gradio.Textbox(label='Prompt'),
