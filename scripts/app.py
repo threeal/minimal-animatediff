03153205b83942c15ceda520def1b028fca903d4
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-07-21 19:34:24 +0800

    feat(app): add several inputs options

--- scripts/app.py
+++ scripts/app.py
@@ -8,25 +8,33 @@ from deps.AnimateDiff.animatediff.utils.util import save_videos_grid
 if __name__ == '__main__':
     pipeline = create_animation_pipeline()
 
-    def generate(prompt, progress=gradio.Progress()):
+    def generate(
+        prompt,
+        neg_prompt,
+        steps,
+        width,
+        height,
+        frames,
+        progress=gradio.Progress()
+    ):
         torch.manual_seed(16372571278361863751)
 
         def update(i, t, latents):
-            progress(i / float(26), desc='Sampling')
+            progress(i / float(steps + 1), desc='Sampling')
 
         progress(0, desc='Sampling')
         sample = pipeline(
             prompt,
-            negative_prompt= '',
-            num_inference_steps = 25,
+            negative_prompt= neg_prompt,
+            num_inference_steps = int(steps),
             guidance_scale = 7.5,
-            width = 512,
-            height = 512,
-            video_length = 16,
+            width = int(width),
+            height = int(height),
+            video_length = int(frames),
             callback = update
         ).videos
 
-        progress(25 / float(26), desc='Converting')
+        progress(steps / float(steps + 1), desc='Converting')
         save_videos_grid(sample, 'samples/sample.gif')
 
         progress(1)
@@ -34,7 +42,14 @@ if __name__ == '__main__':
 
     app = gradio.Interface(
         fn=generate,
-        inputs=gradio.Textbox(label='Prompt'),
+        inputs=[
+            gradio.Textbox(label='Prompt'),
+            gradio.Textbox(label='Negative Prompt'),
+            gradio.Number(label='Steps', value=25, minimum=1),
+            gradio.Number(label='Width', value=512, minimum=1),
+            gradio.Number(label='Height', value=512, minimum=1),
+            gradio.Number(label='Frames', value=16, minimum=1),
+        ],
         outputs=gradio.Image(label='Sample')
     )
 
