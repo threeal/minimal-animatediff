39520fefb5760eb07119fb752182ff9492762a72
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-07-25 18:26:28 +0800

    test: check for saved animation hash

--- tests/test_animate.py
+++ tests/test_animate.py
@@ -1,8 +1,11 @@
+from filehash import FileHash
 import pytest
 import torch
 
 from deps.AnimateDiff.animatediff.utils.util import save_videos_grid
 from modules.animation_pipeline import create_animation_pipeline
+
+hasher = FileHash('md5')
 
 
 @pytest.mark.dependency()
@@ -27,6 +30,11 @@ def test_run_animation_pipeline():
 
 
 @pytest.mark.dependency(depends=['test_run_animation_pipeline'])
-def test_save_animation_pipeline():
+def test_save_animation():
     global sample
     save_videos_grid(sample, 'samples/sample.gif')
+
+
+@pytest.mark.dependency(depends=['test_save_animation'])
+def test_check_saved_animation_hash():
+    assert hasher.hash_file('samples/sample.gif') == '4d69e165159180fba534e4db9a395925'
