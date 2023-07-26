5bbe0f82addcd332ebfa77bf7b02f568a90f9dff
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-07-26 13:31:40 +0800

    style: fix source code formatting

--- minimal_animatediff/diffusion_model.py
+++ minimal_animatediff/diffusion_model.py
@@ -5,19 +5,19 @@ from safetensors import safe_open
 
 from minimal_animatediff.utils import get_model_path
 
-__model_file = get_model_path('toonyou_beta3.safetensors')
+__model_file = get_model_path("toonyou_beta3.safetensors")
 
 
 def load_model():
-    print('Checking diffusion model...')
+    print("Checking diffusion model...")
     if not os.path.isfile(__model_file):
-        print('Diffusion model not found, downloading...')
+        print("Diffusion model not found, downloading...")
         opener = urllib.request.build_opener()
-        opener.addheaders = [('User-Agent', 'MyApp/1.0')]
+        opener.addheaders = [("User-Agent", "MyApp/1.0")]
         urllib.request.install_opener(opener)
-        urllib.request.urlretrieve('http://civitai.com/api/download/models/78775', __model_file)
+        urllib.request.urlretrieve("http://civitai.com/api/download/models/78775", __model_file)
 
-    print('Loading diffusion model state dictionary...')
+    print("Loading diffusion model state dictionary...")
 
     state_dict = {}
     with safe_open(__model_file, framework="pt", device="cpu") as model:
