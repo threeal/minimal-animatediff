f2a18cc62f658ceba490661fbb3f496079c1c125
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-07-31 16:20:59 +0800

    chore: remove unused `get_model_path` utility function

--- minimal_animatediff/utils.py
+++ minimal_animatediff/utils.py
@@ -3,12 +3,6 @@ import os
 from huggingface_hub import snapshot_download
 
 
-def get_model_path(model_name: str):
-    cache_path = os.environ.get("ANIMATEDIFF_MODELS_CACHE")
-    models_path = cache_path if cache_path is not None else "models"
-    return os.path.join(models_path, model_name)
-
-
 def populate_snapshot(path: str) -> str:
     """
     Populates the snapshot of a specified path from the 'threeal/AnimateDiffMirrors' repository.
